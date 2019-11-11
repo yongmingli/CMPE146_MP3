f5cdfdf46912405e561e502bc135ae44ab0f7a91
YongmingLi <yongming.li@sjsu.edu>
2019-11-11 08:21:02 -0800

    env setup

---
+++ lpc40xx_freertos/l5_application/main.c
@@ -0,0 +1,40 @@
+#include "FreeRTOS.h"
+#include "acceleration.h"
+#include "apds.h"
+#include "app_cli.h"
+#include "board_io.h"
+#include "cli_handlers.h"
+#include "clock.h"
+#include "common_macros.h"
+#include "delay.h"
+#include "diskio.h"
+#include "event_groups.h"
+#include "ff.h"
+#include "gpio.h"
+#include "i2c.h"
+#include "lpc40xx.h"
+#include "lpc_peripherals.h"
+#include "queue.h"
+#include "semphr.h"
+#include "sensors.h"
+#include "sj2_cli.h"
+#include "task.h"
+#include "uart.h"
+#include "uart_printf.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+  Lab use
+  Global functions and variables
+*/
+
+int main(void) {
+  printf("Welcome to our MP3!!!\n");
+
+//  sj2_cli__init();
+//  vTaskStartScheduler(); // This function never returns unless RTOS scheduler runs out of memory and fails
+
+  return -1; // return 0;
+}
